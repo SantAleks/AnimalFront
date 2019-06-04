49edfd88e2a7878248b5d42c1d01a0745dc069db
Черенцов Алексей <st_aleks@rambler.ru>
2019-06-04 13:46:08 +0700

    Инициализация Spring
    Signed-off-by: Черенцов Алексей <st_aleks@rambler.ru>

---
+++ src/main/java/com/cherentsov/AnimalFront/AnimalFrontApplication.java
@@ -0,0 +1,13 @@
+package com.cherentsov.AnimalFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cherentsov/AnimalFront/AnimalFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherentsov.AnimalFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
